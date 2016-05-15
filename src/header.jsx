313f957387fa1c7a002b6211206aefcd8e4f7a7c
ck <thecurrencykid@gmail.com>
2016-05-15 13:31:09 -0700

    s5 l29

--- src/header.jsx
+++ src/header.jsx
@@ -2,14 +2,35 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return {
+      text: '' 
+    }
+  },
   render: function(){
     return <div className= "input-group">  
-     <input type="text" className="form-control" />
+     <input 
+     value = {this.state.text} 
+     onChange = {this.handleInputChange}
+     type="text" className="form-control" />
        <span className="input-group-btn">
-        <button className="btn btn-default" type="button"> 
+        <button
+        onClick = {this.handleClick}  
+        className="btn btn-default" type="button"> 
           Add
         </button>
       </span>
+      {this.state.text} 
     </div> 
+  }, 
+
+  handleClick: function(){
+    // send value of text input to firebase
+    console.log(this.state.text); 
+  },
+
+  handleInputChange : function(event){
+    //whatever this event to just happened to, give me a DOM node reference to it
+       this.setState({ text: event.target.value });
   }
 });
