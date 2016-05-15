1c77e9ef331c65a121874673bdf1e1b71d39ac91
ck <thecurrencykid@gmail.com>
2016-05-14 19:47:36 -0700

    first

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('react-fire');
+var Firebase = require('firebase'); 
 
 var Hello = React.createClass({
   render: function() {
