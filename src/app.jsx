d7d8ca7128dd704d37d64ac1faa7a6f3f1b4463e
ck <thecurrencykid@gmail.com>
2016-05-15 12:11:49 -0700

    connect to firebase

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactFire = require('react-fire');
+var ReactFire = require('reactfire');
 var Firebase = require('firebase'); 
+var rootUrl = 'https://firstreacttodos.firebaseio.com/' 
 
-var Hello = React.createClass({
+var App= React.createClass({
+  // with mixins we can take any methods in ReactFire and copy/paste them to our component
+  mixins: [ReactFire],
+  componentWillMount: function(){
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    // this.state.items => {}
+  },
+  // whenever data changes, this.state changes and causes re-render of component
   render: function() {
+    console.log(this.state); 
     return <h1 className="red">
       Hello!
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
+var element = React.createElement(App, {});
 ReactDOM.render(element, document.querySelector('.container'));
