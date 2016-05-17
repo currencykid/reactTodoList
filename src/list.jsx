0f9f93594d2fef3dd887c72b31116e8cb109cf39
ck <thecurrencykid@gmail.com>
2016-05-17 13:41:51 -0700

    s5 l31

--- src/list.jsx
+++ src/list.jsx
@@ -3,14 +3,26 @@ var React = require('react');
 module.exports = React.createClass({
   render: function(){
     return <ul> 
-    {this.renderList()}  
+    {this.renderList()} 
     </ul> 
   }, 
   renderList: function(){
     if(this.props.items && Object.keys(this.props.items).length === 0){
       return <h4> 
-      Add a todo item
+      Add a todo to get started.
       </h4> 
+    } else {
+      var children = [];
+
+      for(var key in this.props.items){
+        children.push(
+          <li>
+            {this.props.items[key].text}
+          </li>
+        )
+      }
+
+      return children; 
     }
   }
 });
